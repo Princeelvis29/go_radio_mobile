fac8e119c40394f6b7d238b9b8bc5f8547c11035
Princeelvis29 <princeelvis29@gmail.com>
2026-07-21 04:35:05 +0100

    Fix MainActivity engine crash for background audio

--- android/app/src/main/kotlin/com/example/go_radio_mobile/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/go_radio_mobile/MainActivity.kt
@@ -1,5 +1,7 @@
 package com.example.go_radio_mobile
 
 import io.flutter.embedding.android.FlutterActivity
+import com.ryanheise.audioservice.AudioServiceActivity
 
-class MainActivity : FlutterActivity()
+class MainActivity: AudioServiceActivity() {
+}
