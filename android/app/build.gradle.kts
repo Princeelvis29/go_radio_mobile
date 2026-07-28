896506f2def3eca0c5fafffe5ae7611b1189f16c
Princeelvis29 <princeelvis29@gmail.com>
2026-07-28 03:28:14 +0100

    Update Android applicationId to com.arktech.goradio

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 }
 
 android {
-    namespace = "com.example.go_radio_mobile"
+    namespace = "com.arktech.goradio"
     compileSdk = 36
     ndkVersion = flutter.ndkVersion
 
@@ -16,7 +16,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.go_radio_mobile"
+        applicationId = "com.arktech.goradio"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         
